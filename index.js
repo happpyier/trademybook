cf369ea7adfd53e50ee55ee331838f86130838b6
unknown <thomas@UBCINC.local>
2017-05-30 10:32:55 -0400

    Starting up new Project

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express'); 
+var app = express();
+var pg = require('pg');
+const https = require('https');
+const fs = require('fs');
+var path = require("path");
+var url = require("url");
+app.set('port', (process.env.PORT || 5000));
+app.set("Content-Type", "text/html");
+app.get([''], function(request, response) {
+	
+});
+app.get(['/reloadPage'], function(request, response) {
+	fs.readFile('reloadPage.html', 'utf8', function (err,data) {
+		response.write(data);
+		response.end();
+	});
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port')); 
+});
