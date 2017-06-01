78ce694527d73f421dcfcbc914816a5e2256dce9
happpyier <happpyier@gmail.com>
2017-06-01 13:33:04 -0400

    Fixing the signup Page

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.get(['/addLogin/:id'], function(request, response) {
 				//response.end();
 			}
 			done();
+			
 		});
-		response.write(testSQlValue1 + "...Test_Results");
 		if (testSQlValue1 > 0)
 		{
 			
@@ -54,6 +54,7 @@ app.get(['/addLogin/:id'], function(request, response) {
 				done();
 			});
 		}
+		response.write(testSQlValue1 + "...Test_Results");
 		response.end();
 		// else
 		// {
