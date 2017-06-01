620ec97a0a7ca2a576e529e5fd2eb4b8d6fa2b80
happpyier <happpyier@gmail.com>
2017-06-01 11:32:39 -0400

    Fixing the signup Page

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ app.get(['/addLogin/:id'], function(request, response) {
 			else
 			{ 
 				testSQlValue1 = JSON.stringify(result.rows.length);
-				
+				response.write(testSQlValue1 + "...Test_Results");
 			}
 			done();
 		});
-		response.write(testSQlValue1 + "...Test_Results");
+		
 		// if (testSQlValue1 > 0)
 		// {
 			// var postSqlCustom2 = "INSERT INTO user_table (name, email, password) VALUES ('"+userName+"', '"+userEmail+"', '"+userPass+"')";
