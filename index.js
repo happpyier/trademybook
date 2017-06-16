bf80a9b533e2b10da48db83270b35a171a71bcba
happpyier <happpyier@gmail.com>
2017-06-16 14:59:31 -0400

    User Settings

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ app.get(['/usersettings'], function(request, response) {
 		});
 		fs.readFile('usersettings.html', 'utf8', function (err,data) {
 			
-			response.write(data + "<div class='testData'>" + testData + "</div>");
+			response.write(data + "<div class='testData'>Hello..." + testData + "...Goodbye</div>");
 			response.end();
 		});
 	}
