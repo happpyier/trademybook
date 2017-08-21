7821a64d4d7792d1437f61eefd1f220a1c91f661
happpyier <happpyier@gmail.com>
2017-08-21 16:35:16 -0400

    mybooks load the database

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.get(['/iframe/loadData'], function(request, response) {
 				{ endValue = ("Error " + err);}
 				else
 				{ 
-					endValue = result.rows[0];
+					endValue = result.rows.length;
 				}
 				done();
 			});
