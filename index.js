00dc3deb2feb9359e30bc7bf5e2bf609d90e0d6c
happpyier <happpyier@gmail.com>
2017-08-28 19:59:25 -0400

    Google Books Api Testing

--- index.js
+++ index.js
@@ -369,7 +369,7 @@ app.get(['/mybooks'], function(request, response) {
 		//{
 			for (var i=0; i<randId_length; i++)
 			{
-				response.write("<div style='display: inline-block;'>" + randId_split[i] + "<img src='" + results.length + "'></img> </div>");	
+				response.write("<div style='display: inline-block;'>" + randId_split[i] + results.length + "<img src='" + " " + "'></img> </div>");	
 			}
 		//}
 		else
