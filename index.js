e437baf1e00f9c0084157c380848e447e8188555
happpyier <happpyier@gmail.com>
2017-08-29 12:35:43 -0400

    Google Book Api DB

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.get(['/addBook/:id'], function(request, response) {
 				BookPassValue_val = "";
 			}
 		});
-		if (BookPassValue > 0)
+		if (BookPassValue.length > 0)
 		{	
 			pg.connect(process.env.DATABASE_URL, function(err, client, done) 
 			{
